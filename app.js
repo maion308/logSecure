500e066a83e5def09d8641b72ff70dd4f0b2dca9
Michae Ion <michael.ion.mi@outlook.com>
2022-09-07 16:57:54 -0400

    set ejs views created login register page forms connected to app.get working

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const express = require('express')
+const app = express()
 const port = process.env.PORT || 3000
 
-express().listen(port, () => console.log('server is running'))
+
+app.set('view engine', 'ejs')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.render('home', {heading: 'Home'})
+})
+
+app.get('/login', (req, res) => {
+    res.render('login', {heading: 'Login'})
+})
+
+app.get('/register', (req, res) => {
+    res.render('register', {heading: 'Register'})
+})
+
+
+
+app.listen(port, () => console.log('server is running'))
